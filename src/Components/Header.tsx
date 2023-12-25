414501ff97311559c1ec8582a2611377d78b173e
vinothM <vinodmad@34gmail.com>
2023-12-25 21:31:48 +0530

    banner update with light anime

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '../Styles/Header.css'
 function Header() {
+  const [bgmode,setbgmode]=useState(false);
   let str=`I am  a web developer from Cape Breton, Nova Scotia and currently living in New York. I enjoy building everything
   from small business sites to rich interactive web apps.`;
   const typing_txt=React.useRef<HTMLElement>(null);
@@ -21,17 +22,46 @@ function Header() {
       len--;
         if(typing_txt.current && typing_txt.current.innerHTML == ''){
           clearInterval(typing_fn);
-          type_text('writing',50)
+          type_text('writing',100)
         }
     }
   }, duration);
   }
-  type_text('writing',50)
+  useEffect(()=>{
+    type_text('writing',100)
+  },[])
+
+  //toggle Mode
+  const toggleBtn=React.useRef<HTMLLIElement>(null);
+  const light=React.useRef<HTMLImageElement>(null);
+  const banner_img=useRef<HTMLElement>(null);
+    if(bgmode){
+      toggleBtn.current?.classList.add('active');
+      light.current?.classList.add('active');
+      setTimeout(() => {
+        light.current?.classList.remove('active');
+        setTimeout(()=>{
+          light.current?.classList.add('active');
+        },300)
+      }, 300);
+      setTimeout(() => {
+        light.current?.classList.remove('active');
+        setTimeout(()=>{
+          light.current?.classList.add('active');
+        },500)
+      }, 800);
+      banner_img.current?.classList.add('active');
+    }else{
+      toggleBtn.current?.classList.remove('active');
+      light.current?.classList.remove('active');
+      banner_img.current?.classList.remove('active');
+    }
+   
   return (
     <section className='banner-wrapper'> 
-        <section className="banner-bg-image">
+        <section className="banner-bg-image" ref={banner_img}>
         <header className='header-container'>
-            <section className='header-title'><span><img src="https://ik.imagekit.io/hs4gfzmpd/vinoth-madhavan-high-resolution-logo-transparent.png?updatedAt=1703439377994" alt="" height={40} width={40}/></span>
+            <section className='header-title'><span><img src="https://ik.imagekit.io/hs4gfzmpd/vm-high-resolution-logo-transparent%20(1).png?updatedAt=1703513209656" alt="" height={40} width={60}/></span>
             <span className="logo-wrapper">
             <span className="logo-title">Vinoth Madhavan</span><br/>
             <span className="logo-designation">Web Developer</span>
@@ -39,12 +69,14 @@ function Header() {
             </section>
             <nav className='nav-wrapper'>
                 <li className="nav-items">Home</li>
-                <li className="nav-items">Contact</li>
+                <li className="nav-items">About</li>
                 <li className="nav-items">Projects</li>
-                <li className="nav-items">Contact US</li>
+                <li className="nav-items">Contact</li>
+                <li className="toggle-btn" onClick={()=>setbgmode(mode=>(!mode))} ref={toggleBtn}></li>
             </nav>
         </header>
         <main className="main-wrapper" data-aos='fade-up' data-aos-duration="2000">
+          <section className="main-content-wrapper">
           <h3 className="banner-header">Websites done right.</h3>
           <p className="banner-para">
           <span className="typing-txt" ref={typing_txt}></span>
@@ -52,6 +84,16 @@ function Header() {
           <section className="banner-btns">
           <button className="hire-me">Hire Me</button>
           <button className="contact">Contact </button>
+        </section>
+        </section>
+
+        <section className="main-image-wrapper">
+          <div className="main-image-container">
+            <img src="https://ik.imagekit.io/hs4gfzmpd/lamp.png?updatedAt=1703504205795" alt="lamp" className='lamp'/>
+          </div>
+          <div className="main-image-container">
+            <img src="https://ik.imagekit.io/hs4gfzmpd/light.png?updatedAt=1703504205803" alt="light" className='light' ref={light}/>
+          </div>
         </section>
         </main>
        
